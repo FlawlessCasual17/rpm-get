6d063c246b62a11c0ca25ab69728c5f7886411de
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-05-01 18:45:40 -0600

    Added real functionality to `getReleases`
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,47 +1,62 @@
 package cmd
 
 import (
+    "encoding/json"
     "flag"
     "fmt"
+    "net/http"
     "os"
     "os/exec"
     "path/filepath"
     "runtime"
+    "strings"
 
     h "github.com/FlawlessCasual17/rpm-get/helpers"
+    "github.com/samber/lo"
     "github.com/spf13/cobra"
 )
-
-// BEGIN: Important variables/constants
 
 // VERSION is the current version of _rpm-get_.
 const VERSION string = "0.0.1"
 
-var homeDir, _ = os.UserHomeDir()
-// CacheDir is the directory where rpm-get will
-// cache JSON files from **_GitHub_**/**_GitLab_**.
-// As well as downloaded packages.
-var CacheDir = filepath.Join(homeDir, ".cache/rpm-get")
+var (
+    // private variables
+
+
+    // public variables
+
+    // CacheDir is the directory where rpm-get will
+    // cache JSON files from **_GitHub_**/**_GitLab_**.
+    // As well as downloaded packages.
+    CacheDir = filepath.Join(os.Getenv("HOME"), ".cache/rpm-get")
+    Project = ""
+    RelType = ""
+    Creator = ""
+    ProjectId = ""
+    // UserAgent is the user agent string used for HTTP requests.
+    UserAgent = fmt.Sprintf(
+        "Mozilla/5.0 (X11; Linux %s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        HOST_CPU)
+    GhHeaderAuth = fmt.Sprintf("Bearer %s", getEnv("GITHUB_TOKEN"))
+    GlHeaderAuth = getEnv("GITLAB_TOKEN")
+)
 
 const ETC_DIR string = "/etc/rpm-get"
 
 // HOST_CPU is the host CPU architecture.
 const HOST_CPU string = runtime.GOARCH
 
-// UserAgent is the user agent string used for HTTP requests.
-var UserAgent = fmt.Sprintf(
-    "Mozilla/5.0 (X11; Linux %s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36", HOST_CPU)
-
-// END: Important variables/constants
-
+// Success exit code
 const SUCCESS_EXIT_CODE int = 0
+// Error exit code
 const ERROR_EXIT_CODE int = 1
+// Usage exit code
 const USAGE_EXIT_CODE int = 2
 
 var rootCmd = &cobra.Command {
     Use: "rpm-get",
     Short: "rpm-get is a CLI tool for downloading and managing RPM packages.",
-    Long: `rpmget is a CLI tool for aquiring RPM packages that are not convieniently
+    Long: `rpm-get is a CLI tool for aquiring RPM packages that are not convieniently
     available in the default repositories. These can be either 3rd party repositories
     or direct download packages from the internet.`,
     Run: func(cmd *cobra.Command, _ []string) {
@@ -115,6 +130,14 @@ func init() {
 
 // spellcheck: ignore
 
+// getEnv returns the value of the environment variable. Empty string if not found.
+func getEnv(key string) string {
+    v, ok := os.LookupEnv(key)
+    return lo.Ternary(ok, v, "")
+}
+
+// spellcheck: ignore
+
 // getVersion prints the current version of rpm-get.
 func getVersion() { fmt.Printf("rpm-get version: %s\n", VERSION) }
 
@@ -154,6 +177,140 @@ func createEtcDir() {
     if err != nil { h.Printc("Unable to create etc dir!", h.FATAL, false) }
 }
 
+// spellcheck: ignore
+// # Gets the releases from either GitHub or GitLab
+// def get_releases
+//   c_file_path = "#{$CACHE_DIR}/#{$app}_cache.json"
+//   url = ''
+//   headers = {}
+//   feedback_msg = nil
+//
+//   case $type
+//   when 'github'
+//     base_url = 'https://api.github.com/repos'
+//     url = "#{base_url}/#{$creator}/#{$app}/releases/latest"
+//     headers = {
+//       'User-Agent': $USER_AGENT,
+//       'Authorization': $gh_header_auth
+//     }
+//     feedback_msg = parse_json(c_file_path, ['message'])
+//   when 'gitlab'
+//     base_url = 'https://gitlab.com/api/v4/projects'
+//     url = "#{base_url}/#{$project_id}/releases/permalink/latest"
+//     headers = {
+//       'User-Agent': $USER_AGENT,
+//       'PRIVATE-TOKEN': $gl_header_auth
+//     }
+//     feedback_msg = File.read(c_file_path)
+//   end
+//
+//   # Ensure cache directory exists
+//   create_cache_dir() unless Dir.exist?($CACHE_DIR)
+//   # If cache file exists, skip
+//   return if File.exist?(c_file_path)
+//
+//   # Notify the user
+//   printc "Updating #{c_file_path}", 'info'
+//   printc "Downloading JSON cache of #{$app} to #{c_file_path}", 'progress'
+//
+//   # Try downloading, or raise an error
+//   begin
+//     Down.download(
+//       url,
+//       destination: c_file_path,
+//       headers: headers,
+//       # @param [Integer] content_length
+//       content_length_proc: lambda do |content_length|
+//         # If `content_length` is null, change the format
+//         content_length.nil? &&
+//           $format = $format.sub('TOTAL::total_byte', '').sub('/:total', '')
+//
+//         bar = TTY::ProgressBar.new($format, total: content_length, head: '>')
+//         bar.resize(50)
+//         $progress_bar = bar
+//       end,
+//       # @param [Float] progress
+//       progress_proc: lambda do |progress|
+//         raise TypeError if progress.nil?
+//
+//         $progress_bar&.advance(progress - $progress_bar.current)
+//       end
+//     )
+//
+//     printc 'Download complete!', 'progress', true
+//   rescue StandardError => e
+//     printc "Failed to update cache for #{$app}", 'error', true
+//     printc e.detailed_message, 'error'
+//   end
+//
+//   # If the GitHub/GitLab API rate limit is exceeded, tell the user
+//   return unless rate_limited?(feedback_msg)
+//
+//   printc 'API rate limit exceeded!', 'warn'
+//   printc "Deleting #{c_file_path}", 'info'
+//   FileUtils.remove_file(c_file_path)
+// end
+// ⬆️ Convert this from Ruby to Go
+
 // getReleases retrieves the list of releases from the GitHub/GitLab API.
-// func getReleases() {
-// }
+func getReleases() {
+    filePath := fmt.Sprintf("%s_cache.json", Project)
+    cacheFilePath := filepath.Join(CacheDir, filePath)
+    url := ""
+    feedbackMsg := ""
+
+    req, _ := http.NewRequest("", url, nil)
+    req.Header.Set("User-Agent", UserAgent)
+
+    switch RelType {
+    case "github":
+        baseUrl := "https://api.github.com/repos"
+        url = fmt.Sprintf("%s/%s/%s/releases/latest", baseUrl, Creator, Project)
+        req.Header.Set("Authorization", GhHeaderAuth)
+        feedbackMsg = parseJson(cacheFilePath, rateLimitedMsg {})
+    case "gitlab":
+        baseUrl := "https://gitlab.com/api/v4/projects"
+        url = fmt.Sprintf("%s/%s/releases/permalink/latest", baseUrl, ProjectId)
+        req.Header.Set("PRIVATE-TOKEN", GlHeaderAuth)
+        v, _ := os.ReadFile(cacheFilePath); feedbackMsg = string(v)
+    }
+
+    if _, e := os.Stat(CacheDir); e != nil && os.IsNotExist(e) { createCacheDir() }
+
+    lo.TryCatch(func() error {
+        resp, _ := http.DefaultClient.Do(req)
+
+        // TODO: Add the user agent string to the request
+        // TODO: Add the authorization header to the request
+        return nil
+    }, func() {
+        h.Printc("Unable to create cache file!", h.FATAL, false)
+    })
+}
+
+type rateLimitedMsg struct {
+    message string
+}
+
+// TODO: Add handling for more complex JSON queries
+
+// parseJson parses the JSON file at the given path.
+func parseJson(filePath string, jsonPath interface{}) string {
+    switch obj := jsonPath.(type) {
+    case rateLimitedMsg:
+        data, _ := os.ReadFile(filePath)
+
+        err := json.Unmarshal(data, &obj)
+
+        if err != nil { h.Printc(err.Error(), h.FATAL, false) }
+
+        return obj.message
+    default:
+        return ""
+    }
+}
+
+func rateLimited(feedbackMsg string) bool {
+    targets := []string { "API rate limit exceeded", "API rate limit exceeded for" }
+    return strings.Contains(feedbackMsg, targets[0]) || strings.Contains(feedbackMsg, targets[1])
+}
