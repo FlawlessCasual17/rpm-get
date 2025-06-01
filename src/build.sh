5a56fb21a233b22866e260234e79b72de8573a30
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-06-01 16:22:28 -0600

    Update `src/build.sh` and `.gitignore`
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd )"
-
-env GOOS='linux' GOARCH='amd64' go build -a -v -o "$SCRIPT_DIR/build/rpm-get"
+env GOOS='linux' GOARCH='amd64' go build -a -v -o "$SCRIPT_DIR/../bin/rpm-get"
